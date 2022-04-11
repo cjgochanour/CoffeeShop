5f7cd4cb882f14a6e59863d93a00503396ecac76
cjgochanour <cjgochanour@gmail.com>
2022-04-11 10:22:59 -0500

    added GetAll coffeerepo method

--- CoffeeShop/Models/Coffee.cs
+++ CoffeeShop/Models/Coffee.cs
@@ -1,10 +1,15 @@
-﻿namespace CoffeeShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
 {
     public class Coffee
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         public string Title { get; set; }
+        [Required]
         public int BeanVarietyId { get; set; }
-        public BeanVariety BeanVariet { get; set; }
+        public BeanVariety BeanVariety { get; set; }
     }
 }
---
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public class CoffeeRepository
+    {
+        private readonly string _connectionString;
+        public CoffeeRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private SqlConnection Connection
+        {
+            get { return new SqlConnection(_connectionString); }
+        }
+
+        public List<Coffee> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id, c.Title, c.BeanVarietyId,
+                                               bv.[Name], bv.Region, bv.Notes
+                                        FROM Coffee c
+                                        LEFT JOIN BeanVariety bv ON bv.Id = c.BeanVarietyId";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var coffees = new List<Coffee>();
+                        while (reader.Read())
+                        {
+                            var coffee = new Coffee()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                                BeanVariety = new BeanVariety()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                                    Region = reader.GetString(reader.GetOrdinal("Region")),
+                                    Notes = reader.IsDBNull(reader.GetOrdinal("Notes")) ? null : reader.GetString(reader.GetOrdinal("Notes"))
+                                }
+                            };
+                            coffees.Add(coffee);
+                        }
+                        return coffees;
+                    }
+                }
+            }
+        }
+    }
+}
