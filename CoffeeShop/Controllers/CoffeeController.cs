1b93d7afb7026e0562b75a500de243ef65ca8cda
cjgochanour <cjgochanour@gmail.com>
2022-04-11 11:19:44 -0500

    added several methods to coffee controller

--- CoffeeShop/Controllers/CoffeeController.cs
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -14,5 +14,43 @@ namespace CoffeeShop.Controllers
         {
             _coffeeRepo = coffeeRepository;
         }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_coffeeRepo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var coffee = _coffeeRepo.Get(id);
+            if (coffee == null)
+            {
+                return NotFound();
+            }
+            return Ok(coffee);
+        }
+        [HttpPost]
+        public IActionResult Post(Coffee coffee)
+        {
+            _coffeeRepo.Add(coffee);
+            return CreatedAtAction("Get", new { id = coffee.Id }, coffee);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Coffee coffee)
+        {
+            if (id != coffee.Id)
+            {
+                return BadRequest();
+            }
+            _coffeeRepo.Update(coffee);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _coffeeRepo.Delete(id);
+            return NoContent();
+        }
     }
 }
