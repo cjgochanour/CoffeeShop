c3cc1fba01bd14e1f94747b10b9ca8d5888729c9
cjgochanour <cjgochanour@gmail.com>
2022-04-11 11:15:37 -0500

    added coffee controller

---
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models;
+using CoffeeShop.Repositories;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        private readonly ICoffeeRepository _coffeeRepo;
+        public CoffeeController(ICoffeeRepository coffeeRepository)
+        {
+            _coffeeRepo = coffeeRepository;
+        }
+    }
+}
