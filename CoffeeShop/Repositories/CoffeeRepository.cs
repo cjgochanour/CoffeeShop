0d83eb9f7773da0541a3a5949064cf140674445d
cjgochanour <cjgochanour@gmail.com>
2022-04-11 10:31:45 -0500

    added Delete method to coffeerepo

--- CoffeeShop/Repositories/CoffeeRepository.cs
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -126,5 +126,18 @@ namespace CoffeeShop.Repositories
                 }
             }
         }
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Coffee WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
